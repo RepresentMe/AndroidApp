28c7ae5c58853ac00f6dc9b0365428f6cd8363f9
Eduard Albu <eduard.albu@gmail.com>
2016-05-06 01:09:29 +0300

    added push notification, reamain to check why server returns invalid token

--- app/src/main/java/com/softrangers/represent/MainActivity.java
+++ app/src/main/java/com/softrangers/represent/MainActivity.java
@@ -1,29 +1,39 @@
 package com.softrangers.represent;
 
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.ViewGroup;
+import android.os.Looper;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.webkit.JavascriptInterface;
-import android.webkit.JsResult;
-import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
-import android.webkit.WebStorage;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import java.util.Map;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String BASE_URL = "https://test.represent.me";
+    private static final String TOKEN_URL = "https://represent.me/api-push/device/gcm/";
     private static final String AUTH = "/auth/login/";
     private static final String FB_AUTH = "/auth/token-only-social-auth/";
     private WebView mWebView;
@@ -34,9 +44,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebView = (WebView) findViewById(R.id.webView);
-//        mWebView.setWebChromeClient(new MyWebViewClient());
-        mWebView.setWebViewClient(new WebViewVlient());
-        mWebView.addJavascriptInterface(new WebViewInterface(), "AndroidErrorReporter");
+        mWebView.setWebChromeClient(new MyWebViewClient());
+        mWebView.setWebViewClient(new WebClient());
+        EditorInfo editorInfo = new EditorInfo();
+        mWebView.onCreateInputConnection(editorInfo);
+        MyJavaScriptInterface javaInterface = new MyJavaScriptInterface();
+        mWebView.addJavascriptInterface(javaInterface, "HTMLOUT");
 
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
         mProgressBar.setMax(100);
@@ -52,28 +65,29 @@ public class MainActivity extends AppCompatActivity {
         mWebView.loadUrl(BASE_URL);
     }
 
-    private class WebViewVlient extends WebViewClient {
-
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            super.onPageStarted(view, url, favicon);
-            if (url.equalsIgnoreCase(BASE_URL + AUTH)) {
-
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (mWebView.canGoBack()) {
+                        mWebView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
             }
         }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class WebClient extends WebViewClient {
 
         @Override
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
-            if (url.equalsIgnoreCase(BASE_URL + AUTH) || url.equalsIgnoreCase(BASE_URL + FB_AUTH)) {
-                WebStorage storage = WebStorage.getInstance();
-                storage.getOrigins(new ValueCallback<Map>() {
-                    @Override
-                    public void onReceiveValue(Map value) {
-
-                    }
-                });
-            }
+            mWebView.loadUrl("javascript:( function () { var resultSrc = localStorage.getItem('auth_token');" +
+                    " window.HTMLOUT.onTokenReady(resultSrc); } ) ()");
         }
     }
 
@@ -83,41 +97,63 @@ public class MainActivity extends AppCompatActivity {
         public void onProgressChanged(WebView view, int newProgress) {
             MainActivity.this.setValue(newProgress);
             super.onProgressChanged(view, newProgress);
-        }
 
-        @Override
-        public void onExceededDatabaseQuota(String url, String databaseIdentifier, long currentQuota, long estimatedSize, long totalUsedQuota, WebStorage.QuotaUpdater quotaUpdater) {
-            //Double the estimated size of the Database
-            quotaUpdater.updateQuota(estimatedSize * 2);
         }
     }
 
-    private class WebViewInterface{
-
+    class MyJavaScriptInterface {
         @JavascriptInterface
-        public void onError(String error){
-            throw new Error(error);
+        public void onTokenReady(String jsResult) {
+            if (jsResult != null && !jsResult.equals("")) {
+                sendPushTokenToServer(jsResult);
+            }
         }
     }
 
-    private void callJavaScript(String methodName, Object...params){
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("javascript:try{");
-        stringBuilder.append(methodName);
-        stringBuilder.append("(");
-        for (int i = 0; i < params.length; i++) {
-            Object param = params[i];
-            if(param instanceof String){
-                stringBuilder.append("'");
-                stringBuilder.append(param);
-                stringBuilder.append("'");
-            }
-            if(i < params.length - 1){
-                stringBuilder.append(",");
-            }
+    private void sendPushTokenToServer(String authToken) {
+        try {
+            final String pushToken = PreferenceManager.getDefaultSharedPreferences(this).getString(Preferences.PUSH_TOKEN_READY, "");
+            if (pushToken.equals("")) return;
+            final String deviceId = "Android_" + pushToken;
+            JSONObject object = new JSONObject();
+            object.put("name", deviceId);
+            object.put("registration_id", pushToken);
+            object.put("active", true);
+            MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+            RequestBody body = RequestBody.create(mediaType, object.toString());
+            Request request = new Request.Builder()
+                    .post(body)
+                    .addHeader("Authorization", "Token " + authToken)
+                    .url(TOKEN_URL)
+                    .build();
+            new OkHttpClient().newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    Looper.prepare();
+                    try {
+                        String body = response.body().string();
+                        String message = new JSONObject(body).optString("detail", "no details");
+                        if (message.equals("no details")) {
+                            PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit()
+                                    .putBoolean(Preferences.SENT_TOKEN_TO_SERVER, true).apply();
+                        }
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    } finally {
+                        Looper.loop();
+                    }
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        stringBuilder.append(")}catch(error){Android.onError(error.message);}");
-        mWebView.loadUrl(stringBuilder.toString());
     }
 
     public void setValue(int progress) {
---
+++ app/src/main/java/com/softrangers/represent/Preferences.java
@@ -0,0 +1,10 @@
+package com.softrangers.represent;
+
+/**
+ * Created by eduard on 06.05.16.
+ */
+public class Preferences {
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+    public static final String PUSH_TOKEN_READY = "pushTokenReady";
+}
