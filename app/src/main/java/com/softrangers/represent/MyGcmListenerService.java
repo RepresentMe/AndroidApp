2a0753646f26da8fe42d6d1f0017021b72d0af44
Eduard Albu <eduard.albu@gmail.com>
2016-05-07 02:42:06 +0300

    added push notification, reamain to check why server returns invalid token

--- app/src/main/java/com/softrangers/represent/MyGcmListenerService.java
+++ app/src/main/java/com/softrangers/represent/MyGcmListenerService.java
@@ -9,7 +9,6 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
